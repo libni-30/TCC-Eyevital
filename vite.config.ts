7685edf7772095f85a227b3fc08d3e5829cae329
EA - Libni Santana Da Silva <libni_santana@educadventista.org>
2025-09-04 08:39:22 -0300

    Initial commit: EyeVital React TypeScript project

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true, // Permite acesso externo
+    strictPort: false, // Permite tentar outras portas se 5173 estiver em uso
+  }
 })
